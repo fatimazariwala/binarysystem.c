6cc888885998db66af5accaec08311ddae015cfe
fatimazariwala <82675386+fatimazariwala@users.noreply.github.com>
2021-07-25 19:45:08 +0530

    6 stairs binary system

---
+++ binarysystem.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+       int main(){
+       int i,j;
+
+       for(i=0;i<6;i++){
+            for(j=0;j<=i;j++){
+                if((i+j)%2==0)
+                {
+                    printf("01");
+                }
+                else{
+                    printf("01");
+                }
+            }printf("\n");
+       }
+
+
+   return 0;}
